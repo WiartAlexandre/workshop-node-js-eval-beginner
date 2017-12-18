a163506bc1587eedbcc1a98e75d672c14c036197
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2017-12-18 16:34:28 +0100

    Fix input for test exercise 2

--- test/exercise2.test.js
+++ test/exercise2.test.js
@@ -5,7 +5,7 @@ const exercise = require("../src/exercise2");
 describe("Exercise #2", function() {
   describe("function run()", function() {
     it("should return a number which is the sum of numbers written in a string delimited by comma given in input", function() {
-      expect(exercise.run(['64,23,56,7'])).to.be.eql(150);
+      expect(exercise.run("64,23,56,7")).to.be.eql(150);
     });
   });
 });
